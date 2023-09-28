66e4081c67177812b8b1488fa4f13f9d3a651360
Iris10763 <94052867+Iris10763@users.noreply.github.com>
2023-09-28 15:06:07 +0200

    Ajout des interfaces
    Interfaces brouillons

---
+++ src/main/java/fr/ufrsciencestech/interfaces/JavaApplicationTest.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplicationtest;
+
+/**
+ *
+ * @author mgall
+ */
+public class JavaApplicationTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Interface().setVisible(true);
+            }
+        });
+        
+        
+        
+    }
+    
+}
