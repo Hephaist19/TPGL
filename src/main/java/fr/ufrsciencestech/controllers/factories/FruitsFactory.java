5c7c802857a1f20f64709ec6dd0c28602713b236
Nathan J <jacquinet.nathan@gmail.com>
2023-11-01 11:28:06 +0100

    Mise a jour FruitFactory

--- src/main/java/fr/ufrsciencestech/controllers/factories/FruitsFactory.java
+++ src/main/java/fr/ufrsciencestech/controllers/factories/FruitsFactory.java
@@ -1,13 +1,21 @@
 
 package fr.ufrsciencestech.controllers.factories;
 
+import java.util.ArrayList;
+
 import fr.ufrsciencestech.models.fruits.*;
 
 enum FruitType {
     ANANAS,
+    BANANE,
+    CERISE,
+    CITRON,
+    FRAISE,
     FRAMBOISE,
     KIWI,
+    LITCHI,
     ORANGE,
+    PECHE,
     POMME
 }
 
@@ -21,12 +29,24 @@ public class FruitsFactory {
         switch (type) {
             case ANANAS:
                 return new Ananas();
+            case BANANE:
+                return new Banane();
+            case CERISE:
+                return new Cerise();
+            case CITRON:
+                return new Citron();
+            case FRAISE:
+                return new Fraise();
             case FRAMBOISE:
                 return new Framboise();
             case KIWI:
                 return new Kiwi();
+            case LITCHI:
+                return new Litchi();
             case ORANGE:
                 return new Orange();
+            case PECHE:
+                return new Peche();
             case POMME:
                 return new Pomme();
             default: //normalement ce cas ne se déclanchera jamais car nous utilisons une enumeration pour lister tous les fruits disponible
@@ -38,16 +58,43 @@ public class FruitsFactory {
         switch (type) {
             case ANANAS:
                 return new Ananas(prix, origine);
+            case BANANE:
+                return new Banane(prix, origine);
+            case CERISE:
+                return new Cerise(prix, origine);
+            case CITRON:
+                return new Citron(prix, origine);
+            case FRAISE:
+                return new Fraise(prix, origine);
             case FRAMBOISE:
                 return new Framboise(prix, origine);
             case KIWI:
                 return new Kiwi(prix, origine);
+            case LITCHI:
+                return new Litchi(prix, origine);
             case ORANGE:
                 return new Orange(prix, origine);
+            case PECHE:
+                return new Peche(prix, origine);
             case POMME:
                 return new Pomme(prix, origine);
             default: //normalement ce cas ne se déclanchera jamais car nous utilisons une enumeration pour lister tous les fruits disponible
                 return null;
         }
+    }
+
+    /**
+     * Crée une liste contenant tous les fruits disponible
+     * @return ArrayList de Fruit
+     */
+    public static ArrayList<Fruit> createAll() {
+
+        ArrayList<Fruit> result = new ArrayList<>();
+
+        for(FruitType t : FruitType.values()) {
+            result.add(createFruit(t));
+        }
+
+        return result;
     }
 }
