6943877bb4a81cf9b27e7504f375ad10de5c7c77
Hephaist19 <julie.lavauzelle@gmail.com>
2023-11-04 10:37:41 +0100

    modif FruitsFilter

--- src/main/java/fr/ufrsciencestech/controllers/factories/PanierFactory.java
+++ src/main/java/fr/ufrsciencestech/controllers/factories/PanierFactory.java
@@ -1,45 +1,45 @@
 package fr.ufrsciencestech.controllers.factories;
 
-import fr.ufrsciencestech.controllers.FilterExecutor;
+import fr.ufrsciencestech.controllers.FruitsFilter;
 import fr.ufrsciencestech.models.*;
 import fr.ufrsciencestech.utils.*;
 import fr.ufrsciencestech.models.fruits.Fruit;
 import java.util.*;
 
 public class PanierFactory {
-     
-    public static Panier createPanier(int contenance){
+
+    public static Panier createPanier(int contenance) {
         return new Panier(contenance);
-    }  
-    
-    public static Panier createPanier(PanierType type){
-       
+    }
+
+    public static Panier createPanier(PanierType type) {
+
         Panier p;
-        FilterExecutor fe;
+        FruitsFilter fe;
         ArrayList<Fruit> list;
-        
-        switch(type){
-            case EXOTIQUE :
-                list = FruitsFactory.createAllOf(FruitType.ANANAS,FruitType.BANANE,FruitType.LITCHI,FruitType.KIWI);
+
+        switch (type) {
+            case EXOTIQUE:
+                list = FruitsFactory.createAllOf(FruitType.ANANAS, FruitType.BANANE, FruitType.LITCHI, FruitType.KIWI);
                 p = new Panier(list.size());
                 p.setFruits(list);
                 break;
-            case AGRUME :
-                list = FruitsFactory.createAllOf(FruitType.ORANGE,FruitType.CITRON);
+            case AGRUME:
+                list = FruitsFactory.createAllOf(FruitType.ORANGE, FruitType.CITRON);
                 p = new Panier(list.size());
                 p.setFruits(list);
                 break;
-            case SANSPEPINS :
+            case SANSPEPINS:
                 list = FruitsFactory.createAll();
-                fe = new FilterExecutor(list);
-                list = fe.filter(FilterType.SANSPEPINS).getResult();
+                fe = new FruitsFilter(list);
+                list = fe.filter(FilterType.NPEPINS).getResult();
                 p = new Panier(list.size());
                 p.setFruits(list);
                 break;
-            case AVECPEPINS :
+            case AVECPEPINS:
                 list = FruitsFactory.createAll();
-                fe = new FilterExecutor(list);
-                list = fe.filter(FilterType.AVECPEPINS).getResult();
+                fe = new FruitsFilter(list);
+                list = fe.filter(FilterType.PEPINS).getResult();
                 p = new Panier(list.size());
                 p.setFruits(list);
                 break;
@@ -49,8 +49,8 @@ public class PanierFactory {
                 p.setFruits(list);
                 break;
         }
-        
+
         return p;
     }
-    
+
 }
---
+++ src/test/java/fr/ufrsciencestech/controllers/FruitsFilterTest.java
@@ -0,0 +1,92 @@
+package fr.ufrsciencestech.controllers;
+
+import fr.ufrsciencestech.controllers.factories.FruitsFactory;
+import fr.ufrsciencestech.models.fruits.Fruit;
+import fr.ufrsciencestech.utils.*;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.*;
+
+public class FruitsFilterTest {
+
+        ArrayList<Fruit> listeTest;
+        FruitsFilter fe;
+
+        public FruitsFilterTest() {
+        }
+
+        @Before
+        public void setUp() {
+                // Setup une liste dans cet ordre:
+                // Kiwi - Banane - Orange - Cerise - Ananas - Framboise
+                listeTest = FruitsFactory.createAllOf(FruitType.KIWI, FruitType.BANANE, FruitType.ORANGE,
+                                FruitType.CERISE,
+                                FruitType.ANANAS, FruitType.FRAMBOISE);
+                fe = new FruitsFilter(listeTest);
+        }
+
+        @Test
+        public void testSort() {
+                System.out.println("testSortAlphabetique");
+                ArrayList<Fruit> result = fe.sort(SortType.ALPHABETIQUE).getResult();
+                ArrayList<Fruit> attendu = FruitsFactory.createAllOf(FruitType.ANANAS, FruitType.BANANE,
+                                FruitType.CERISE,
+                                FruitType.FRAMBOISE, FruitType.KIWI, FruitType.ORANGE);
+                assertEquals(attendu, result);
+
+                System.out.println("testSortAntiAlphabetique");
+                fe = new FruitsFilter(listeTest);
+                result = fe.sort(SortType.ANTIALPHABETIQUE).getResult();
+                attendu = FruitsFactory.createAllOf(FruitType.ORANGE, FruitType.KIWI, FruitType.FRAMBOISE,
+                                FruitType.CERISE,
+                                FruitType.BANANE, FruitType.ANANAS);
+                assertEquals(attendu, result);
+
+                // Teste tri par prix
+                // cf prix constructeur defaut des fruit en question
+                System.out.println("testSortprixCroissant");
+                result = fe.sort(SortType.PRIXCROISSANT).getResult();
+                attendu = FruitsFactory.createAllOf(FruitType.KIWI, FruitType.ORANGE, FruitType.BANANE,
+                                FruitType.ANANAS,
+                                FruitType.FRAMBOISE, FruitType.CERISE);
+                assertEquals(attendu, result);
+
+                System.out.println("testSortprixDécroissant");
+                fe = new FruitsFilter(listeTest);
+                result = fe.sort(SortType.PRIXDECROISSANT).getResult();
+                attendu = FruitsFactory.createAllOf(FruitType.CERISE, FruitType.FRAMBOISE, FruitType.ANANAS,
+                                FruitType.BANANE,
+                                FruitType.ORANGE, FruitType.KIWI);
+                assertEquals(attendu, result);
+
+        }
+
+        @Test
+        public void testFilter() {
+                System.out.println("testFilterSansPepin");
+                // Test filtre SANS PEPINS -> doit laisser que des fruits sans pépins
+                ArrayList<Fruit> result = fe.filter(FilterType.NPEPINS).getResult();
+                ArrayList<Fruit> attendu = FruitsFactory.createAllOf(FruitType.BANANE, FruitType.ORANGE,
+                                FruitType.ANANAS,
+                                FruitType.FRAMBOISE);
+                assertEquals(attendu, result);
+
+                System.out.println("testFilterAvecPepin");
+                // Test filtre SANS PEPINS
+                fe = new FruitsFilter(listeTest);
+                result = fe.filter(FilterType.PEPINS).getResult();
+                attendu = FruitsFactory.createAllOf(FruitType.KIWI, FruitType.CERISE);
+                assertEquals(attendu, result);
+        }
+
+        @Test
+        public void testFruitsFilter() {
+                System.out.println("testFruitsFilter");
+                FruitsFilter fe = new FruitsFilter();
+                assertEquals(fe.getResult().size(), FruitType.values().length);
+        }
+
+}
